2554f3740a783d2a65cea1fb4a999d180a29db2d
Yangyingjie <1977412373@qq.com>
2019-08-19 16:57:35 +0800

    删除了一些没用的方法

--- src/main/java/cn/dao/UserDao.java
+++ src/main/java/cn/dao/UserDao.java
@@ -7,8 +7,4 @@ import org.springframework.stereotype.Repository;
 public interface UserDao {
     //添加一个用户
     boolean add(User user);
-    //根据ID获取用户的时候不应该获取用户的密码
-    User getById(int id);
-    //获取用户的完整信息
-    User get(User user);
 }
